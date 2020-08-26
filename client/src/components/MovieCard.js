2fc7e9c07adeb3ed6c49791d5ede3a92b9c91ed5
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-08-25 21:31:31 -0700

    done for now - deply later

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -20,6 +20,7 @@ const MovieCard = ({ id, title, vote_average, poster_path, overview }) => {
         overflow: "auto",
         padding: "20px",
         borderRadius: "3px",
+        position: "relative",
       }}
     >
       <CardImg
@@ -32,12 +33,24 @@ const MovieCard = ({ id, title, vote_average, poster_path, overview }) => {
         src={`https://image.tmdb.org/t/p/original/${poster_path}`}
         alt="Card image cap"
       />
-      <CardBody>
+      <CardBody style={{ textAlign: "center" }}>
         <CardTitle style={{ textAlign: "center" }}>{title}</CardTitle>
         <CardSubtitle>{vote_average}</CardSubtitle>
         <CardText>{overview}</CardText>
         {/* Button here will link to a new page with synopsis on this movie or open a modal.. not sure yet */}
-        <Button>Button</Button>
+        <a
+          className="btn btn-primary"
+          target="_blank"
+          href={`https://www.themoviedb.org/movie/${id}`}
+          style={{
+            position: "absolute",
+            bottom: "10px",
+            left: "30%",
+            width: "40%",
+          }}
+        >
+          Learn More
+        </a>
       </CardBody>
     </Card>
   );
